e03228b6326a3af8e8384edb1ce53ef3a9e3f2f8
xaldey <fguzkp25@gmail.com>
2021-12-04 11:08:45 +0300

    Добавлен вывод изображений списком. Добавил ссылку на вывод списка в шапку.

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'images'
 
 urlpatterns = [
+    path('', views.image_list, name='list'),
     path('create/', views.image_create, name='create'),
+    path('like/', views.image_like, name='like'),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
 ]
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,8 +1,41 @@
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+# Пока не совсем понятно, почему редактор считает импорт из декораторов некорректным. Однако - работает.
+from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        if request.is_ajax():
+            # If page is not an integer deliver the first page
+            images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images', 'images': images})
 
 
 def image_detail(request, id, slug):
@@ -11,6 +44,25 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})
+
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'error'})
 
 
 @login_required
