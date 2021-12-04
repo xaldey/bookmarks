25e9b9c1e4817242d5d6bfeaf7bdceffccbe8a24
xaldey <fguzkp25@gmail.com>
2021-12-04 08:33:14 +0300

    jQuery выбор картинок со страницы работает.

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -1,12 +1,11 @@
 (function(){
   var jquery_version = '3.4.1';
-  var site_url = 'https://127.0.0.1:8000/';
+  var site_url = 'https://centr-krasnodar.com:8000/';
   var static_url = site_url + 'static/';
   var min_width = 100;
   var min_height = 100;
 
-  function bookmarklet(msq) {
-    // Here goes our bookmarklet code
+  function bookmarklet(msg) {
     // load CSS
     var css = jQuery('<link>');
     css.attr({
@@ -22,44 +21,61 @@
 
     // close event
     jQuery('#bookmarklet #close').click(function(){
-        jQuery('#bookmarklet').remove();
+       jQuery('#bookmarklet').remove();
     });
-
     // find images and display them
     jQuery.each(jQuery('img[src$="jpg"]'), function(index, image) {
-      if (jQuery(image).width() >= min_width && jQuery(image).height() >= min_height)
+      if (jQuery(image).width() >= min_width && jQuery(image).height()
+      >= min_height)
       {
         image_url = jQuery(image).attr('src');
-        jQuery('#bookmarklet .images').append('<a href="#"><img src="' + image_url +'" /></a>');
+        jQuery('#bookmarklet .images').append('<a href="#"><img src="'+
+        image_url +'" /></a>');
       }
     });
+
+    // when an image is selected open URL with it
+    jQuery('#bookmarklet .images a').click(function(e){
+      selected_image = jQuery(this).children('img').attr('src');
+      // hide bookmarklet
+      jQuery('#bookmarklet').hide();
+      // open new window to submit the image
+      window.open(site_url +'images/create/?url='
+                  + encodeURIComponent(selected_image)
+                  + '&title='
+                  + encodeURIComponent(jQuery('title').text()),
+                  '_blank');
+    });
+
   };
-    // Check if jQuery is loaded
-    if(typeof window.jQuery != 'undefined') {
-      bookmarklet();
-    } else {
-      // Check for conflicts
-      var conflict = typeof window.$ != 'undefined';
-      // Create the script and point to Google API
-      var script = document.createElement('script');
-      script.src = '//ajax.googleapis.com/ajax/libs/jquery/' + jquery_version + '/jquery.min.js';
-      // Add the script to the head for processing
-      document.head.appendChild(script);
-      // Create a way to wait until script loading
-      var attempts = 15;
-      (function(){
-        // Check again if jQuery is undefined
-        if(typeof window.jQuery == 'undefined') {
-          if(--attempts > 0) {
-            // Calls himself in a few milliseconds
-            window.setTimeout(arguments.callee, 250)
-          } else {
-            // Too much attempts to load, send error
-            alert('Ошибка загрузки jQuery')
-          }
+
+  // Check if jQuery is loaded
+  if(typeof window.jQuery != 'undefined') {
+    bookmarklet();
+  } else {
+    // Check for conflicts
+    var conflict = typeof window.$ != 'undefined';
+    // Create the script and point to Google API
+    var script = document.createElement('script');
+    script.src = '//ajax.googleapis.com/ajax/libs/jquery/' +
+      jquery_version + '/jquery.min.js';
+    // Add the script to the 'head' for processing
+    document.head.appendChild(script);
+    // Create a way to wait until script loading
+    var attempts = 15;
+    (function(){
+      // Check again if jQuery is undefined
+      if(typeof window.jQuery == 'undefined') {
+        if(--attempts > 0) {
+          // Calls himself in a few milliseconds
+          window.setTimeout(arguments.callee, 250)
         } else {
-            bookmarklet();
+          // Too much attempts to load, send error
+          alert('An error occurred while loading jQuery')
         }
-      })();
-    }
+      } else {
+          bookmarklet();
+      }
+    })();
+  }
 })()
