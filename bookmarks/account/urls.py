86ae6297a8227b8c64a7432a5b5fdd3c03be3988
xaldey <fguzkp25@gmail.com>
2021-12-11 11:27:37 +0300

    Добавлена система лайков и фолловеров. Также начал внедрение системы отображения действий других пользователей.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -19,6 +19,8 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/', views.user_list, name='user_list'),
     path('users/<username>', views.user_detail, name='user_detail'),
+
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'images.apps.ImagesConfig',
     'easy_thumbnails',
+    'actions.apps.ActionsConfig',
 ]
 
 MIDDLEWARE = [
